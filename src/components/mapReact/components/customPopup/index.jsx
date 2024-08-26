b1d5945e43b332862214aaa2978eb78ad8b5e85c
odilboyev <webiusdev@gmail.com>
2024-08-26 16:06:31 +0500

    popup drag problem fixed

--- src/components/mapReact/components/customPopup/index.jsx
+++ src/components/mapReact/components/customPopup/index.jsx
@@ -1,16 +1,26 @@
 import { memo, useEffect, useRef } from "react";
 import { Popup } from "react-leaflet";
 import SingleRecord from "../singleRecord";
+import L from "leaflet";
 
 const DraggablePopup = memo(function DraggablePopup({
   marker = {},
   onClose = () => {},
   id,
 }) {
+  const popupRef = useRef(null);
   return (
     <Popup
-      // ref={popupRef}
-      eventHandlers={{ close: onClose }}
+      eventHandlers={{
+        mouseover: (e) => {
+          // console.log('over',e.target.getElement())
+          const element = e.target.getElement();
+          const draggable = new L.Draggable(element);
+          draggable.enable();
+        },
+      }}
+      ref={popupRef}
+      // eventHandlers={{ close: onClose }}
       maxWidth={"100%"}
       interactive
       closeOnClick={false}
