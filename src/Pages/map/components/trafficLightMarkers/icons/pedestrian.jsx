e656129a252e6574b78a181a547e00c238946e28
odilboyev <webiusdev@gmail.com>
2025-01-04 11:33:43 +0500

    pedestrian icon improved on trafficlight markers

--- src/Pages/map/components/trafficLightMarkers/icons/pedestrian.jsx
+++ src/Pages/map/components/trafficLightMarkers/icons/pedestrian.jsx
@@ -1,45 +1,66 @@
 const PedestrianIcons = () => (
   <svg
     version="1.1"
-    viewBox="0 0 2048 2048"
-    width="5em"
+    viewBox="0 0 3000 2048"
+    width="8em"
     height="5em"
     xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor" // Ensures the paths are filled
+    fill="currentColor"
   >
     {/* Pedestrian Icon 1 */}
-    {/* <path
+    <path
       transform="translate(0,0)"
-      fill="currentColor" // Explicitly filling the path
-      d="M0 0h180l9 2 9 4 9 7 8 11 4 12v866l-3 11-6 10-5 5-7 6-10 4-16 2h-166l-13-2-10-4-12-11-6-10-3-11-1-95v-688l1-81 3-11 5-9 8-9 10-6 5-2z"
-    /> */}
+      fill="currentColor"
+      d="M0 0h120l6 2 6 4 6 7 5 11 3 12v866l-2 11-4 10-3 5-5 6-7 4-11 2h-111l-9-2-7-4-8-11-4-10-2-11-1-95v-688l1-81 2-11 3-9 5-9 7-6 3-2z"
+    />
 
     {/* Pedestrian Icon 2 */}
     <path
-      transform="translate(450,0)"
-      fill="currentColor" // Explicitly filling the path
-      d="M0 0h180l9 2 9 4 9 7 8 11 4 12v866l-3 11-6 10-5 5-7 6-10 4-16 2h-166l-13-2-10-4-12-11-6-10-3-11-1-95v-688l1-81 3-11 5-9 8-9 10-6 5-2z"
+      transform="translate(350,0)"
+      fill="currentColor"
+      d="M0 0h120l6 2 6 4 6 7 5 11 3 12v866l-2 11-4 10-3 5-5 6-7 4-11 2h-111l-9-2-7-4-8-11-4-10-2-11-1-95v-688l1-81 2-11 3-9 5-9 7-6 3-2z"
     />
 
     {/* Pedestrian Icon 3 */}
     <path
-      transform="translate(900,0)"
-      fill="currentColor" // Explicitly filling the path
-      d="M0 0h180l9 2 9 4 9 7 8 11 4 12v866l-4 13-7 11-10 8-10 4-16 2h-166l-13-2-10-4-12-11-6-10-3-11-1-96v-687l1-81 3-11 5-9 8-9 10-6 5-2z"
+      transform="translate(700,0)"
+      fill="currentColor"
+      d="M0 0h120l6 2 6 4 6 7 5 11 3 12v866l-3 13-5 11-7 8-7 4-11 2h-111l-9-2-7-4-8-11-4-10-2-11-1-96v-687l1-81 2-11 3-9 5-9 7-6 3-2z"
     />
 
     {/* Pedestrian Icon 4 */}
     <path
-      transform="translate(1350,0)"
-      fill="currentColor" // Explicitly filling the path
-      d="M0 0h181l12 3 10 6 3 2v2h2l7 10 4 11 1 7v855l-2 14-7 13-9 9-13 6-16 2h-166l-14-2-12-6-7-6-7-10-4-11-1-859 2-15 6-12 9-10 10-6 5-2z"
+      transform="translate(1050,0)"
+      fill="currentColor"
+      d="M0 0h120l8 3 7 6 2 2v2h1l5 10 3 11 1 7v855l-1 14-5 13-6 9-9 6-11 2h-111l-9-2-8-6-5-6-5-10-3-11-1-859 1-15 4-12 6-10 7-6 3-2z"
     />
 
     {/* Pedestrian Icon 5 */}
     <path
-      transform="translate(1800,0)"
-      fill="currentColor" // Explicitly filling the path
-      d="M0 0h180l12 3 10 6 3 2v2h2l7 10 4 11 1 7v855l-2 14-7 13-9 9-13 6-16 2h-167l-13-2-12-6-7-6-7-10-4-11-1-858 2-16 6-12 9-10 10-6 5-2z"
+      transform="translate(1400,0)"
+      fill="currentColor"
+      d="M0 0h120l8 3 7 6 2 2v2h1l5 10 3 11 1 7v855l-1 14-5 13-6 9-9 6-11 2h-111l-9-2-8-6-5-6-5-10-3-11-1-858 1-16 4-12 6-10 7-6 3-2z"
+    />
+
+    {/* Pedestrian Icon 6 */}
+    <path
+      transform="translate(1750,0)"
+      fill="currentColor"
+      d="M0 0h120l8 3 7 6 2 2v2h1l5 10 3 11 1 7v855l-1 14-5 13-6 9-9 6-11 2h-111l-9-2-8-6-5-6-5-10-3-11-1-858 1-16 4-12 6-10 7-6 3-2z"
+    />
+
+    {/* Pedestrian Icon 7 */}
+    <path
+      transform="translate(2100,0)"
+      fill="currentColor"
+      d="M0 0h120l8 3 7 6 2 2v2h1l5 10 3 11 1 7v855l-1 14-5 13-6 9-9 6-11 2h-111l-9-2-8-6-5-6-5-10-3-11-1-858 1-16 4-12 6-10 7-6 3-2z"
+    />
+
+    {/* Pedestrian Icon 8 */}
+    <path
+      transform="translate(2450,0)"
+      fill="currentColor"
+      d="M0 0h120l8 3 7 6 2 2v2h1l5 10 3 11 1 7v855l-1 14-5 13-6 9-9 6-11 2h-111l-9-2-8-6-5-6-5-10-3-11-1-858 1-16 4-12 6-10 7-6 3-2z"
     />
   </svg>
 );
